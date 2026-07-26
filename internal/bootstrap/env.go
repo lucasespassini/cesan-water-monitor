70ec9734b9d08127b5265dc8e24d83447c4f2898
Lucas Mendes <lucasespassini@gmail.com>
2026-07-26 00:11:47 -0300

    refactor: env vars

--- internal/bootstrap/env.go
+++ internal/bootstrap/env.go
@@ -2,31 +2,30 @@ package bootstrap
 
 import (
 	"log"
+	"os"
 
-	"github.com/spf13/viper"
+	"github.com/joho/godotenv"
 )
 
 type Env struct {
-	AGazetaCesanUrl string `mapstructure:"AGAZETA_CESAN_URL"`
-	CronInterval    string `mapstructure:"CRON_INTERVAL"`
-	RedisAddress    string `mapstructure:"REDIS_ADDRESS"`
-	TelegramToken   string `mapstructure:"TELEGRAM_TOKEN"`
-	Environment     string `mapstructure:"ENVIRONMENT"`
+	AGazetaCesanUrl string
+	CronInterval    string
+	RedisAddress    string
+	TelegramToken   string
+	Environment     string
 }
 
 func NewEnv() *Env {
-	env := Env{}
-	viper.SetConfigFile(".env")
-
-	err := viper.ReadInConfig()
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Can't find the file .env : ", err)
+		log.Fatal("Error loading .env file")
 	}
 
-	err = viper.Unmarshal(&env)
-	if err != nil {
-		log.Fatal("Environment can't be loaded: ", err)
+	return &Env{
+		AGazetaCesanUrl: os.Getenv("AGAZETA_CESAN_URL"),
+		CronInterval:    os.Getenv("CRON_INTERVAL"),
+		RedisAddress:    os.Getenv("REDIS_ADDRESS"),
+		TelegramToken:   os.Getenv("TELEGRAM_TOKEN"),
+		Environment:     os.Getenv("ENVIRONMENT"),
 	}
-
-	return &env
 }
